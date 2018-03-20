e22f52ed065911a2bca51cdb35b62507b36cd7ad
Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>
2018-03-20 17:23:09 +0000

    EID-403: Build from map using library make function
    - Don't need to encode logic for a making anchors in
      another class – only the CountryTrustAnchor class
      needs to worry about that.
    - Dependency on security utils can be pushed
      to tests.
    - Renames to make variables clearer.

--- src/main/java/uk/gov/ida/eidas/trustanchor/Generator.java
+++ src/main/java/uk/gov/ida/eidas/trustanchor/Generator.java
@@ -3,21 +3,15 @@ package uk.gov.ida.eidas.trustanchor;
 import java.security.PrivateKey;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSObject;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
-import net.minidev.json.JSONObject;
-import uk.gov.ida.common.shared.security.X509CertificateFactory;
 
 public class Generator {
   private final JWKSetSigner signer;
@@ -26,35 +20,21 @@ public class Generator {
     this.signer = new JWKSetSigner(signingKey, null, certificate);
   }
 
-  public JWSObject generateFromMap(Map<String, String> trustAnchorMap) throws ParseException, JOSEException, CertificateEncodingException {
-    List<String> inputFiles = trustAnchorMap.entrySet().stream()
-            .map(entry -> buildJsonObject(entry.getKey(), entry.getValue()))
-            .collect(Collectors.toList());
-
-    return generate(inputFiles);
-  }
-
-  public JWSObject generate(List<String> inputFiles) throws JOSEException, ParseException, CertificateEncodingException {
-    List<JWK> certs = new ArrayList<>();
-    for (String input : inputFiles) {
-      certs.add(CountryTrustAnchor.parse(input));
+  public JWSObject generateFromMap(Map<String, X509Certificate> trustAnchorMap) throws ParseException, JOSEException, CertificateEncodingException {
+    List<JWK> keys = new ArrayList<>();
+    for (Map.Entry<String, X509Certificate> entry : trustAnchorMap.entrySet()) {
+      keys.add(CountryTrustAnchor.make(Arrays.asList(entry.getValue()), entry.getKey()));
     }
 
-    return signer.sign(new JWKSet(certs));
+    return signer.sign(new JWKSet(keys));
   }
 
-  private String buildJsonObject(String keyId, String certificate) {
-    X509Certificate x509Certificate = new X509CertificateFactory().createCertificate(certificate);
-    RSAPublicKey publicKey = (RSAPublicKey) x509Certificate.getPublicKey();
-    JSONObject jsonObject = new JSONObject();
-    jsonObject.put("kty", "RSA");
-    jsonObject.put("key_ops", Collections.singletonList("verify"));
-    jsonObject.put("kid", keyId);
-    jsonObject.put("alg", "RS256");
-    jsonObject.put("e", new String (Base64.getEncoder().encode(publicKey.getPublicExponent().toByteArray())));
-    jsonObject.put("n", new String (Base64.getEncoder().encode(publicKey.getModulus().toByteArray())));
-    jsonObject.put("x5c", Collections.singletonList(certificate));
+  public JWSObject generate(List<String> inputJSONs) throws JOSEException, ParseException, CertificateEncodingException {
+    List<JWK> keys = new ArrayList<>();
+    for (String input : inputJSONs) {
+      keys.add(CountryTrustAnchor.parse(input));
+    }
 
-    return jsonObject.toJSONString();
+    return signer.sign(new JWKSet(keys));
   }
 }
