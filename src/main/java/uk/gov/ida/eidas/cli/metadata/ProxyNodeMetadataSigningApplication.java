43dc07eb1e4c3bf576085c7bff9caa88befc6e7e
dbes-gds <38218115+dbes-gds@users.noreply.github.com>
2019-01-30 14:48:10 +0000

    EID-1254: Sign Proxy Node Metadata
    verify-metadata calls the trust-anchor library to sign eidas connector metadata.
    Added an additional command to sign eidas proxy node metadata.  This reuses the connector metadata code which already signs with an hsm or smartcard.  The additional option makes usage explicit and prevents making any changes to working connector metadata signing.

---
+++ src/main/java/uk/gov/ida/eidas/cli/metadata/ProxyNodeMetadataSigningApplication.java
@@ -0,0 +1,17 @@
+package uk.gov.ida.eidas.cli.metadata;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name="proxy-node-metadata", description="Signs Proxy Node Metadata", subcommands={
+    SignWithFile.class,
+    SignWithSmartcard.class
+})
+public class ProxyNodeMetadataSigningApplication implements Runnable {
+    @Override
+    public void run() {
+        // If we reach this point, we didn't match any subcommands.
+        // So print the usage; there's nothing to do by default.
+        CommandLine.usage(this, System.err);
+    }
+}
